fdb523e98eb0fc9a7af231b94941965ec65110ce
Marius Cristea <marius.cristea@irian.ro>
2018-03-08 23:45:59 +0200

    L07 - NG - Route Lazy - init - commons

---
+++ L07_NL/PizzaApp/src/app/commons/commons.module.ts
@@ -0,0 +1,14 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {NavigationServiceProvider} from "./navigation.service";
+
+@NgModule({
+  imports: [
+    CommonModule,
+    CommonsModule
+  ],
+  declarations: [],
+  providers: [NavigationServiceProvider]
+})
+export class CommonsModule {
+}
