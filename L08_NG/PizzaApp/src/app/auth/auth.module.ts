b649e0041321f95959a7f589c4314621ed7e6a12
Marius Cristea <marius.cristea@irian.ro>
2018-03-11 01:16:50 +0200

    L08 - NG - Route Guards - init - auth module

--- L08_NG/PizzaApp/src/app/auth/auth.module.ts
+++ L08_NG/PizzaApp/src/app/auth/auth.module.ts
@@ -1,11 +1,14 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 
+import {AuthServiceProvider} from "./auth.service";
+
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [],
+  providers: [AuthServiceProvider]
 })
 export class AuthModule {
 }
